5578ec5b127988f28e9f6463b1f8c69adddb35d6
Julian Curalli <julian.curalli27@gmail.com>
2026-02-05 22:21:53 -0500

    fix build

--- src/app/match-card/page.tsx
+++ src/app/match-card/page.tsx
@@ -1,10 +1,15 @@
 import MatchCard from "@/components/match-card/MatchCard";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Match Card",
 };
 
 export default function MatchCardPage() {
-  return <MatchCard />;
+  return (
+    <Suspense fallback={<div className="min-h-screen" />}>
+      <MatchCard />
+    </Suspense>
+  );
 }
